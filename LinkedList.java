e2b8a889438fee4b8728b582cc1dbb3142fd7886
Brian <bmccormack15@gmail.com>
2021-12-18 02:36:37 -0800

    implemented insert and print menthods

--- LinkedList.java
+++ LinkedList.java
@@ -7,12 +7,38 @@ public class LinkedList {
         this.tail = null;
     }
 
-    public int insert() {
-        return 1;
-
+    //insert at back of the list to start
+    public void insert(LinkedListNode newItem) {
+        //case empty list
+        if (head == null)
+        {
+            this.head = newItem;
+            this.tail = newItem;
+            return;
+        }
+        
+        //case at least on item is currently in list
+        LinkedListNode current = head;
+        while(current.getNext() != null)
+            current = current.getNext();
+        current.setNext(newItem);
+        this.tail = newItem;
     }
 
     public void delete() {
 
+    }
+
+    public void print() {
+        //case empty list
+        if(head == null)
+            return;
+        
+        LinkedListNode current = head;
+        while(current != null)
+        {
+            System.out.println(current.getValue());
+            current = current.getNext();
+        }
     }
 }
--- LinkedListCaller.java
+++ LinkedListCaller.java
@@ -1,7 +1,12 @@
 public class LinkedListCaller {
     public static void main(String []args) {
+        int insertVal = 5;
         LinkedList items = new LinkedList();
         System.out.println("This is a linked List Skeleton");
-        System.out.println(items.insert());
+
+        //LinkedListNode a = new LinkedListNode(insertVal);
+        for(int i = 0; i < 10; i++)
+            items.insert(new LinkedListNode(i));
+        items.print();
     }
 }
